66507a080c4829d00932fcbbed290b5a288ad62b
Elazar Gershuni <elazarg@gmail.com>
2025-11-30 02:19:05 +0200

    no global

--- src/main/kotlin/vegas/dag/FrontierMachine.kt
+++ src/main/kotlin/vegas/dag/FrontierMachine.kt
@@ -9,41 +9,8 @@ class FrontierMachine<T : Any> private constructor(
     /** Currently enabled nodes. */
     fun enabled(): Set<T> = enabled
 
-    /** All nodes that have not yet been resolved. */
-    fun unresolved(): Set<T> = unresolved
-
-    /** Number of unresolved prerequisites of n. */
-    fun remainingDepsOf(n: T): Int = remainingDeps[n] ?: 0
-
     /** All nodes have been resolved. */
     fun isComplete(): Boolean = unresolved.isEmpty()
-
-    /**
-     * Resolve one enabled node and return a new FrontierMachine.
-     * Does not mutate the current instance.
-     */
-    fun resolve(node: T): FrontierMachine<T> {
-        require(node in enabled) { "Node $node is not enabled." }
-
-        val newUnresolved = unresolved - node
-        val newEnabled: MutableSet<T> = (enabled - node).toMutableSet()
-        val newRemainingDeps = remainingDeps.toMutableMap()
-
-        dependents[node]?.forEach { d ->
-            if (d in newUnresolved) {
-                val c = newRemainingDeps.getValue(d) - 1
-                newRemainingDeps[d] = c
-                if (c == 0) newEnabled.add(d)
-            }
-        }
-
-        return FrontierMachine(
-            dependents = dependents,
-            unresolved = newUnresolved,
-            enabled = newEnabled,
-            remainingDeps = newRemainingDeps
-        )
-    }
 
     /**
      * Resolve all enabled node and return a new FrontierMachine.
